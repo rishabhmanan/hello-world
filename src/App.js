31871192a0e0527ff0e3b22a858b5c2814976760
Rishabh Manan <rishabhmanan21@gmail.com>
2024-03-18 17:13:04 +0530

    introduced setState

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from "./components/greet"
 import Hello from "./components/hello"
+import Message from "./components/message"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name="Test" heroName="Batman"/>
-        <Greet name="User" heroName="Superman"/>
-        <Greet name="Name" heroName="Flash"/>
+        {/* <Greet name="Test" heroName="Batman"/> */}
+        {/* <Greet name="User" heroName="Superman"/> */}
+        {/* <Greet name="Name" heroName="Flash"/> */}
         {/* <Hello/> */}
+        <Message />
       </div>
     )
   }
